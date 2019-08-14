23cf82254c6de85df0c8b38c9f596e2c80ec620d
Matt Fehskens <matt.fehskens@gmail.com>
2019-08-14 00:23:08 -0400

    Use latest talc
    - Add an index template for posts
    - Move templates into talc directory
    - Set a date format

--- talc.config.js
+++ talc.config.js
@@ -1,5 +1,7 @@
 module.exports = {
+  dateFormat: "M/d/yyyy hh:mm a",
+  index: "talc/index.html",
   input: "posts",
   output: "posts-html",
-  template: "post-template.html"
+  template: "talc/post.html"
 };
