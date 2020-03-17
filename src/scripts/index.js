60d9044df5f0b6e6ed6ce88f410c8340f2841d14
Matt Fehskens <matt.fehskens@gmail.com>
2020-03-16 23:55:36 -0400

    Added night mode

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,63 +1,93 @@
-'use strict';
+"use strict";
 
 (function() {
-    var sectionHeaders = document.querySelectorAll('section > header');
+  window.onload = function() {
+    var sectionHeaders = document.querySelectorAll("section > header");
     var headerArray = Array.prototype.slice.call(sectionHeaders);
+    var nightmode = document.querySelector("input#nightmode");
 
-    headerArray.forEach(function (header) {
-        header.addEventListener('mouseover', randomizeHeaderColor);
-        header.addEventListener('mouseout', resetHeaderColor);
+    headerArray.forEach(function(header) {
+      header.addEventListener("mouseover", randomizeHeaderColor);
+      header.addEventListener("mouseout", resetHeaderColor);
 
-        header.addEventListener('touchstart', touchHeaderColor);
+      header.addEventListener("touchstart", touchHeaderColor);
     });
 
-    function touchHeaderColor(event) {
-        var header = event.target;
+    nightmode.addEventListener("click", toggleNightMode);
+  };
 
-        if (event.target.classList.contains('randomizing')) {
-            resetHeaderColor(event);
-        } else {
-            randomizeHeaderColor(event);
-        }
+  function touchHeaderColor(event) {
+    var header = event.target;
+
+    if (event.target.classList.contains("randomizing")) {
+      resetHeaderColor(event);
+    } else {
+      randomizeHeaderColor(event);
     }
+  }
 
-    function randomizeHeaderColor(event) {
-        var header = event.target;
+  function randomizeHeaderColor(event) {
+    var header = event.target;
 
-        header.classList.add('randomizing');
+    header.classList.add("randomizing");
+    changeHeaderColor(header);
+  }
+
+  function changeHeaderColor(header) {
+    var color = [null, null, null, null, null, null].map(pickHex);
+
+    header.style.backgroundColor = "#" + color.join("");
+
+    setTimeout(function() {
+      if (header.classList.contains("randomizing")) {
         changeHeaderColor(header);
+      }
+    }, 250);
+  }
+
+  function pickHex() {
+    var hex = [
+      "0",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F"
+    ];
+    // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+    var digit = Math.floor(Math.random() * (15 - 1));
+
+    return hex[digit];
+  }
+
+  function resetHeaderColor(event) {
+    var header = event.target;
+
+    header.classList.remove("randomizing");
+    header.style.backgroundColor = "";
+    header.style.backgroundImage = "";
+  }
+
+  function toggleNightMode(event) {
+    var target = event.target;
+    var icon = target.previousElementSibling;
+
+    document.body.classList.toggle("nightmode");
+
+    if (target.checked) {
+      icon.innerHTML = "🌙";
+    } else {
+      icon.innerHTML = "🌞";
     }
-
-    function changeHeaderColor(header) {
-        var color = [null, null, null, null, null, null].map(pickHex);
-
-        header.style.backgroundColor = '#' + color.join('');
-
-        setTimeout(function() {
-            if (header.classList.contains('randomizing')) {
-                changeHeaderColor(header);
-            }
-        }, 250);
-    }
-
-    function pickHex() {
-        var hex = [
-            '0', '1', '2', '3',
-            '4', '5', '6', '7',
-            '8', '9', 'A', 'B',
-            'C', 'D', 'E', 'F'
-        ];
-        // from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-        var digit = Math.floor(Math.random() * (15 - 1));
-
-        return hex[digit];
-    }
-
-    function resetHeaderColor(event) {
-        var header = event.target;
-
-        header.classList.remove('randomizing');
-        header.style.backgroundColor = '';
-        header.style.backgroundImage = '';
-    }
+  }
 })();
