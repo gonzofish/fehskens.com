51b5567766f183ad2a91b8d94213df724983c615
Matt Fehskens <matt.fehskens@gmail.com>
2020-04-05 17:12:30 -0400

    Change config to generate tag pages

--- talc.config.js
+++ talc.config.js
@@ -1,8 +1,82 @@
+const createTagPages = (files, template) => {
+  const byTag = {};
+  const output = [];
+  const untagged = [];
+
+  for (const file of files) {
+    const { tags } = file;
+
+    if (!tags) {
+      untagged.push(file);
+    } else {
+      for (const tag of tags) {
+        if (!byTag[tag]) {
+          byTag[tag] = [];
+        }
+
+        byTag[tag].push(file);
+      }
+    }
+  }
+
+  if (untagged.length > 0) {
+    tagFiles.untagged = untagged;
+  }
+
+  for (const [tag, tagFiles] of Object.entries(byTag)) {
+    output.push({
+      filename: `tags/${tag}.html`,
+      files: tagFiles,
+      template: "tag.html"
+    });
+  }
+
+  if (output.length > 0) {
+    output.push({
+      filename: "tags.html",
+      files: output
+        .map(({ filename }) => ({
+          filename: filename,
+          title: filename.replace(/(^tags\/|\.html$)/g, "")
+        }))
+        .sort((a, b) => {
+          let direction = -1;
+
+          if (a.title > b.title) {
+            direction = 1;
+          }
+
+          return direction;
+        }),
+      template: "tags.html"
+    });
+  }
+
+  return output;
+};
+
 module.exports = {
   built: "posts-html",
   dateFormat: "M/d/yyyy hh:mm a",
-  index: "talc/index.html",
-  published: "posts",
-  sortBy: ["publish_date"],
-  template: "talc/post.html"
+  pages: {
+    directory: "talc",
+    templates: [
+      {
+        sortBy: ["publish_date"],
+        template: "index.html",
+        type: "listing"
+      },
+      {
+        sortBy: ["publish_date"],
+        template: "index.html",
+        transformer: createTagPages,
+        type: "listing"
+      },
+      {
+        template: "post.html",
+        type: "post"
+      }
+    ]
+  },
+  published: "posts"
 };
